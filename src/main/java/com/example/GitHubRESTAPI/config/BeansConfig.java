ceefe2857fad7f81be319b356dd0504d6b399390
Dhruvan <dhruvankadavala111@gmail.com>
2023-09-07 13:25:37 +0530

    beans configuration resolved

--- src/main/java/com/example/GitHubRESTAPI/config/BeansConfig.java
+++ src/main/java/com/example/GitHubRESTAPI/config/BeansConfig.java
@@ -1,14 +1,21 @@
 package com.example.GitHubRESTAPI.config;
 
+import com.example.GitHubRESTAPI.service.dtoservice.DTOConverter;
+import com.example.GitHubRESTAPI.service.dtoservice.DTOConverterImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.Model;
 
 @Configuration
-public class ModelMapperConfig {
+public class BeansConfig {
     // bean will be required to inject as a dependency
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+    @Bean
+    public DTOConverterImpl dtoConverter(ModelMapper modelMapper){
+        return new DTOConverterImpl(modelMapper);
     }
 }
